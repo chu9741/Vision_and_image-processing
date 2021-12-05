fb195fb02b99657d5f18b19df1e7858fb3e58861
chu9741 <95557557+chu9741@users.noreply.github.com>
2021-12-05 16:58:07 +0900

    Add files via upload

---
+++ Coin_counting/VisionOpenCVHW2.cpp
@@ -0,0 +1,91 @@
+﻿// VisionOpenCVHW2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core.hpp>
+#include <vector>
+#include <math.h>
+
+using namespace cv;
+using namespace std;
+void Find(int num);
+
+int main()
+{
+    int num = 5;
+    for (int i = 0; i <= num; i++) {
+        Find(i);
+        waitKey();
+    }
+    return 0;
+}
+
+void Find(int num) {
+    String path[6] = {"C:/Users/USER/source/repos/VisionOpenCVHW2/Coins/coins0.jpg", 
+        "C:/Users/USER/source/repos/VisionOpenCVHW2/Coins/coins1.jpg","C:/Users/USER/source/repos/VisionOpenCVHW2/Coins/coins2.jpg",
+        "C:/Users/USER/source/repos/VisionOpenCVHW2/Coins/coins3.jpg","C:/Users/USER/source/repos/VisionOpenCVHW2/Coins/coins4.jpg",
+        "C:/Users/USER/source/repos/VisionOpenCVHW2/Coins/coins5.jpg"};
+    
+
+    Mat image = cv::imread(path[num], 0);
+    imshow("Original_image", image);
+    Mat cannyimage;
+    Mat cannyimage2;
+    Mat GBlur;
+    Mat GBlur2;
+    Mat gray;
+    //cvtColor(image,cannyimage2, COLOR_BGR2GRAY);
+    GaussianBlur(image, GBlur2, Size(13, 13),0); // kernel size, sigma 
+    Canny(GBlur2, cannyimage2, 50, 150, 3); // threshold low, high, kernel 
+    imshow("detected circles", cannyimage2);
+    waitKey();
+    vector<Vec3f> circles;
+    HoughCircles(cannyimage2, circles, HOUGH_GRADIENT, 1, cannyimage2.rows / 8, 100, 20, 30, 90); //
+
+    for (size_t i = 0; i < circles.size(); i++)
+    {
+        Vec3i c = circles[i];
+        Point center = Point(c[0], c[1]);
+        // circle center
+
+        circle(cannyimage2, center, 1, Scalar(0, 100, 100), 3, LINE_AA);
+        // circle outline
+        int radius = c[2];
+        circle(cannyimage2, center, radius, Scalar(255, 0, 255), 3, LINE_AA);
+    }
+    //imshow("detected circles", image);
+    //waitKey();
+
+    //imshow("Image", cannyimage);
+    imshow("Counting_Coins", cannyimage2);
+    printf("coin%d.jpg 의 circle개수 : %d\n",num, circles.size());
+    
+}
+
+/*
+두 번째 과제는 영상속에 포함된 동전의 개수를 찾는 프로그램을 구현하는 것입니다.
+아래 입력 파일을 각각 읽고, 적절한 edge detection method를 이용하여 동전의 경계를 찾은 후
+Hough circle transform 등의 방법들을 활용하여 동전의 개수를 출력합니다.
+"Hoguh circle transform manual을 반드시 확인하시고, Canny edge 등의 방법을 먼저 적용할 지의 여부, 각 매개변수의 의미와 용도를 파악하십시오."
+"매개변수 한 값으로 모든 사진에서 맞는 동전의 개수를 찾을 수 있어야 합니다."
+0 - 3번 영상에서는 정확한 개수를 세어야 하고, 4 - 5번째 영상은 정확한 개수가 나오지 않아도 상관 없습니다.
+참고: 동전의 위치를 영상에 그려보면 더 빠르게 적절한 매개변수를 찾을 수 있습니다.
+작성한 소스코드만 제출합니다.
+*/
+
+/*
+1. Filter image with x, y derivatives of Gaussian 
+2. Find magnitude and orientation of gradient
+3. Non-maximum suppression:
+Thin multi-pixel wide “ridges” down to single pixel width
+4. Thresholding and linking (hysteresis):
+Define two thresholds: low and high
+Use the high threshold to start edge curves and the low threshold to continue them
+
+MATLAB: edge(image, ‘canny’)
+OpenCV: Canny( src, dst, lowThreshold, highTreshold, kernel_size )
+                 It needs Gaussian, first
+*/
